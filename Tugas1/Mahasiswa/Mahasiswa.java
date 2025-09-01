cd7933dbd3c4572959c27286d2b38d6e70c957ca
Ridho Arazisyah <ridhoarazyy@gmail.com>
2025-09-01 20:00:21 +0700

    Add files via upload

---
+++ Tugas1/Mahasiswa/Mahasiswa.java
@@ -0,0 +1,15 @@
+package mahasiswa;
+
+public class Mahasiswa {
+    void membaca() {
+        System.out.println("Mahasiswa sedang membaca buku.");
+    }
+
+    void nyontek() {
+        System.out.println("Mahasiswa menyontek AI saat mengerjakan tugas.");
+    }
+
+    void mengerjakan() {
+        System.out.println("Mahasiswa sedang mengerjakan tugas.");
+    }
+}
---
+++ Tugas1/Mahasiswa/MahasiswaBeraksi.java
@@ -0,0 +1,12 @@
+package mahasiswa;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.membaca();
+        mhs.nyontek();
+        mhs.mengerjakan();
+    }
+}
+
