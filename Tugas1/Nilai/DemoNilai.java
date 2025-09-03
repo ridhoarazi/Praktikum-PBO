ef1575aaf484c61bd134f4a32966800989b8eb08
Ridho Arazisyah <ridhoarazyy@gmail.com>
2025-09-03 08:44:50 +0700

    Add files via upload

---
+++ Tugas1/Nilai/DemoNilai.java
@@ -0,0 +1,11 @@
+package nilai;
+
+public class DemoNilai {
+    public static void main(String[] args) {
+        Nilai n = new Nilai();
+        
+        n.Nilai("4410631170001", "Still In Love", 100, 95, 90, 90, 95);
+
+        n.CetakNilai();
+    }
+}
